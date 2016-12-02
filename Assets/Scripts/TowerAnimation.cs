196123de6b2456293c2e9846b92e39d31465ac79
Alexander Scheurer <mail@aspepex.net>
2016-12-02 10:14:09 +0100

    Towers are now animated and animations are at least a bit randomized

---
+++ Assets/Scripts/TowerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private float changetimer;
+    private float changetime;
+
+	// Use this for initialization
+	void Start () {
+	    animator = this.GetComponent<Animator>();
+        animator.Play("Tower", -1, Random.Range(0.0f, 1.0f));
+	    animator.speed = Random.Range(0.7f, 1.3f);
+	    changetime = Random.Range(10f, 20f);
+	    changetimer = changetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (changetimer <= 0)
+	    {
+	        animator.speed = Random.Range(0.7f, 1.3f);
+	        changetimer = changetime;
+	    }
+	}
+}
