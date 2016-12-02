49fa4015e0f9da4c8bc154519f2f9524f264e533
Alexander Scheurer <mail@aspepex.net>
2016-12-02 12:16:11 +0100

    Nameing of Module and size

--- Assets/Scripts/TowerAnimation.cs
+++ Assets/Scripts/TowerAnimation.cs
@@ -5,23 +5,28 @@ public class TowerAnimation : MonoBehaviour
 {
     private Animator animator;
     private float changetimer;
-    private float changetime;
 
 	// Use this for initialization
 	void Start () {
 	    animator = this.GetComponent<Animator>();
         animator.Play("Tower", -1, Random.Range(0.0f, 1.0f));
-	    animator.speed = Random.Range(0.7f, 1.3f);
-	    changetime = Random.Range(10f, 20f);
-	    changetimer = changetime;
-	}
+	    
+        ChangeStuff();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if (changetimer <= 0)
 	    {
-	        animator.speed = Random.Range(0.7f, 1.3f);
-	        changetimer = changetime;
-	    }
+            ChangeStuff();
+        }
+
+	    changetimer -= Time.deltaTime;
 	}
+
+    void ChangeStuff()
+    {
+        animator.speed = Random.Range(0.7f, 1.3f);
+        changetimer = Random.Range(10f, 20f);
+    }
 }
